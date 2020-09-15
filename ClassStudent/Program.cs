3b540be084a65d19e6bf5aab8edd5111f7ebcb2b
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-15 10:05:21 +0700

    Add project files.

---
+++ ClassStudent/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Studen student1 = new Studen(1, "Ho Van Trung", "Hue", 20);
+
+            Studen student2 = new Studen();
+            student2.SetId(2);
+            student2.SetName("Ho Van Trung1");
+            student2.SetAddress("Hue1");
+            student2.SetAge(21);
+
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.Read();
+        }
+    }
+}
---
+++ ClassStudent/Studen.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassStudent
+{
+    class Studen
+    {
+        private int id;
+        private string name;
+        private string address;
+        private int age;
+
+        public Studen() { }
+        public Studen(int id, string name, string address, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.age = age;
+        }
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public void SetId(int _id)
+        {
+            this.id = _id;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string _name)
+        {
+            this.name = _name;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        public void SetAge(int _age)
+        {
+            this.age = _age;
+        }
+
+        public string GetAddress()
+        {
+            return this.address;
+        }
+
+        public void SetAddress(string _address)
+        {
+            this.address = _address;
+        }
+        public override string ToString()
+        {
+            return "Id : " + id + " Name : " + name + " Address : " + address + " Age : " + age;
+        }
+    }
+}
